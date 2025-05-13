1ceed2d5bcfdd730e9d423aceafc99860be6cf9d
BuchaCero <jlbu971107@gmail.com>
2025-05-13 15:24:02 +0800

    style(button): adjust color on hover for better cursor feedback

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-black text-white border border-black shadow-xs hover:bg-gray-800 hover:text-black",
+          "bg-black text-white border border-black shadow-xs hover:bg-white hover:text-black",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
